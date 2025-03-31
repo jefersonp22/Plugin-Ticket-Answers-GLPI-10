794f03b78bfa180c1ad6f07d4a39d864cb687ae1
Jeferson <104242426+jefersonp22@users.noreply.github.com>
2025-03-31 17:48:24 -0300

    Delete locales directory

--- locales/en_US.php
+++
@@ -1,5 +0,0 @@
-<?php
-$LANG['plugin_ticketanswers']['title'] = "Ticket Answers";
-$LANG['plugin_ticketanswers']['config'] = "Configuration";
-$LANG['plugin_ticketanswers']['stats'] = "Statistics";
-?>
--- locales/pt_BR.php
+++
@@ -1,5 +0,0 @@
-<?php
-$LANG['plugin_ticketanswers']['title'] = "Ticket Answers";
-$LANG['plugin_ticketanswers']['config'] = "Configuração";
-$LANG['plugin_ticketanswers']['stats'] = "Estatísticas";
-?>
